9b3d1088650feec5bcde5679e21f36f0e3c84b2f
Joe M <mccarthysound@gmail.com>
2023-06-26 19:00:05 -0400

    Update button handling

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import closeIcon from '../assets/icons/close_FILL0_wght400_GRAD0_opsz48.svg';
 import expandIcon from '../assets/icons/expand_content_FILL0_wght400_GRAD0_opsz48.svg';
-import infoIcon from '../assets/icons/info_FILL0_wght400_GRAD0_opsz48.svg';
+import infoIcon from '../assets/icons/info_modified.svg';
 
 
 type iconType = 'close' | 'expand' | 'info';
