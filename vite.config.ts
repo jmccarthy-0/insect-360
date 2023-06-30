13376b5480ab0ec51715b24e243dffb9ac4a2948
Joe M <mccarthysound@gmail.com>
2023-06-30 19:58:35 -0400

    Fix PostCSS plugin handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import postCSSNested from 'postcss-nested';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        postCSSNested()
+      ],
+    }
+  },
   server: {
     host: true
   }
