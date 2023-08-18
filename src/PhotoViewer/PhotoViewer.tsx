34328e59c6e5f036c9dd36fea85aa182a61d64d2
Joe M <mccarthysound@gmail.com>
2023-08-18 16:53:26 -0400

    Add loader to Photo Viewer

--- src/PhotoViewer/PhotoViewer.tsx
+++ src/PhotoViewer/PhotoViewer.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSingleImageLoader } from '../hooks/singleImageLoader';
 
 import ImageCanvas from '../ImageCanvas/ImageCanvas';
 import ZoomBtns from "../ZoomBtns/ZoomBtns";
 
 import classes from './PhotoViewer.module.css';
+import Loader from '../Loader/Loader';
 
 
 
@@ -12,14 +13,18 @@ import classes from './PhotoViewer.module.css';
 const PhotoViewer = () => {
     const img = useSingleImageLoader('/hi-res/29540763125_daca6ae7f7_o.jpg');
     const [zoomLevel, setZoomLevel] = useState(0);
+    const [displayLoader, setDisplayLoader] = useState(true);
+
+    useEffect(() => {
+        img && img.complete && setDisplayLoader(false);
+    }, [img]);
 
     return (   
-        <>
+        <div className={classes['photo-viewer']}>
+            {displayLoader && <Loader />}
             <ZoomBtns setZoomLevel={setZoomLevel}  zoomLevel={zoomLevel}/>
-            <div className={classes['photo-viewer']}>
-                <ImageCanvas img={img} zoomLevel={zoomLevel} panningEnabled={true} />
-            </div>
-        </>
+            <ImageCanvas img={img} zoomLevel={zoomLevel} panningEnabled={true} />
+        </div>
     )
 }
 
