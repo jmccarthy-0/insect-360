29575486c0e82d809f6a7aaa5c0f63260802310c
Joe M <mccarthysound@gmail.com>
2023-10-10 19:00:53 -0400

    Remove router alias

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
 //import React from 'react';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './Layout';
 import Index from './pages/Index';
@@ -10,14 +10,14 @@ const AppRouter = () => {
     const [displaySpeciesMenu, setDisplaySpeciesMenu] = useState(false);
     
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
                 <Route path={import.meta.env.BASE_URL} element={<Layout displaySpeciesMenu={displaySpeciesMenu} setDisplaySpeciesMenu={setDisplaySpeciesMenu} />}> {/* Replace insect-360 pathing for index once we're off githubpage config*/}
                     <Route index={true} element={<Index setDisplaySpeciesMenu={setDisplaySpeciesMenu} />} />
                     <Route path=":speciesId" element={ <SpeciesDetailsPage /> } />
                 </Route>
             </Routes>
-        </Router>
+        </BrowserRouter>
     )
 }
 
