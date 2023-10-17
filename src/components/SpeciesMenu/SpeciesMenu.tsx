57472cb03b3c1347cc36dc7f33b62ddc8152141a
Joe M <mccarthysound@gmail.com>
2023-10-16 22:20:01 -0400

    Style species list

--- src/components/SpeciesMenu/SpeciesMenu.tsx
+++ src/components/SpeciesMenu/SpeciesMenu.tsx
@@ -2,11 +2,9 @@ import { useState, useEffect, useContext, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { SpeciesMenuContext } from '../../contexts/SpeciesMenuContext';
 import { fetchData } from '../../utils/ts/fetch-utils';
-
 import Modal from '../Modal/Modal';
-import species from '../../assets/species-tree.json';
 import classes from './SpeciesMenu.module.css';
-import btnClasses from '../Btn/Btn.module.css';
+import linkClasses from '../Link/Link.module.css';
 
 type SpeciesItem = {
     sid: string,
@@ -18,11 +16,12 @@ const SpeciesMenu = () => {
     const { displaySpeciesMenu, setDisplaySpeciesMenu} = useContext(SpeciesMenuContext)
     const [speciesTree, setSpeciesTree] = useState< ReactElement | null>(null);
 
+    // Back end data for building list of species
     const buildTree = async () => {
         const data = await fetchData(`http://127.0.0.1:8000/species/`);
 
         return (
-            <ul className={`${classes['taxon-list']}`} >
+            <ul className={`${classes['species-list']}`} >
                 {
                     data.map(({sid, genus, species}: SpeciesItem, index: number) => {
                         const handleClick = () => {
@@ -31,9 +30,7 @@ const SpeciesMenu = () => {
                         
                         return (
                             <li key={index}>
-                                { 
-                                    <Link to={`${sid}/`} className={`${btnClasses['btn--link']}`} onClick={handleClick}>{genus} {species}</Link>
-                                }
+                                <Link to={`${sid}/`} className={`${linkClasses['link']} ${linkClasses['link--italic']}`} onClick={handleClick}>{genus} {species}</Link>
                             </li>
                         );
                     })
@@ -58,6 +55,7 @@ const SpeciesMenu = () => {
         return (
             <Modal id="speciesMenuModal" setOpen={setDisplaySpeciesMenu} animationDirection='fade'> 
                 <div className={classes['species-menu']}>
+                    <h2 className={classes['species-title']}>Species</h2>
                     { speciesTree }
                 </div>
             </Modal>
