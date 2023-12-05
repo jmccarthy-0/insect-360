23823b69eb30f1dc824ef98c4e7cbf91d527217f
Joe M <mccarthysound@gmail.com>
2023-12-04 20:15:52 -0500

    Create loader animation spinner

--- src/components/global/Loader/Loader.tsx
+++ src/components/global/Loader/Loader.tsx
@@ -1,18 +1,9 @@
-import { useEffect } from "react";
-
-import './Loader.css';
+import LoaderStyles from './Loader.module.css';
 
 const Loader = () => {
-    useEffect(() => {
-        console.log('Loader Mount')
-
-        return () => {
-            console.log('Loader Unmount');
-        }
-    }, []);
     return (
-        <div className="loader">
-            <p>Loading Animation Placeholder</p>
+        <div className={`${LoaderStyles['loader']}`}>
+            <span className={`${LoaderStyles['spinner']}`}></span>
         </div>
     );
 }
