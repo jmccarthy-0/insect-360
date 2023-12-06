e8390106f8c2c0b7ea063118e4790719713b77c1
Joe M <mccarthysound@gmail.com>
2023-12-05 21:05:38 -0500

    Fix 404 display

--- src/components/global/Error404/Error404.tsx
+++ src/components/global/Error404/Error404.tsx
@@ -1,10 +1,13 @@
 import classes from './Error404.module.css';
+import LayoutStyles from '@/routes/Layout.module.css';
 
 const Error404 = () => {
     return (
-        <div className={`${classes['error-wrapper']}`}>
-            <h1 className={`${classes['error-title']}`}>404</h1>
-            <p>Page not found.</p>
+        <div className={`${LayoutStyles['page-grid']}`}>
+            <main className={`${classes['error-wrapper']}`}>
+                <h1 className={`${classes['error-title']}`}>404</h1>
+                <p className={`${classes['error-descr']}`}>Page not found.</p>
+            </main>
         </div>
     );
 }
