65c99fdd45f32d00601fba5abf7f1c00763adf4c
Joe M <mccarthysound@gmail.com>
2023-12-05 20:42:54 -0500

    Remove github-specific pathing config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/insect-360/',
+  base: '/',
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
