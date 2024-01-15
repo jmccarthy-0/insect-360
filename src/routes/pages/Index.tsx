59df0c1d48b9a8e43e477406ae95999497933fae
Joe M <mccarthysound@gmail.com>
2024-01-14 19:46:28 -0500

    Add OG meta tags to home and SDP

--- src/routes/pages/Index.tsx
+++ src/routes/pages/Index.tsx
@@ -2,11 +2,20 @@ import {Helmet} from "react-helmet";
 import AppIntro from "@components/pages/home/AppIntro/AppIntro";
 
 const Index = () => {
+    const pageDescription = 'An online resource offering interactive 360° macro image displays of a variety of insect species.';
+
     return (
         <>
             <Helmet>
                 <title>{import.meta.env.VITE_TITLE}</title>
-                <meta name="description" content="An online resource offering interactive 360° macro image displays of a variety of insect species." />
+                <meta name="description" content={pageDescription} />
+
+                {/* Open Graph */}
+                <meta property="og:title" content={import.meta.env.VITE_TITLE}/>
+                <meta property="og:description" content={pageDescription}/>
+                <meta property="og:image" content={`https://lookatbugs.com/species_assets/dolichovespula-maculata/sequence/dolichovespula-maculata_viewer_frame-01.webp`}/>
+                <meta property="og:image:width" content="2400"/>
+                <meta property="og:image:height" content="1800"/>
             </Helmet>
             <AppIntro />
         </>
--- src/routes/pages/SpeciesDetailsPage.tsx
+++ src/routes/pages/SpeciesDetailsPage.tsx
@@ -8,11 +8,21 @@ const SpeciesDetailsPage = () => {
     const species = useLoaderData() as Taxon;
     const {binomialName, commonName} = species.details;
 
+    const pageTitle = `${import.meta.env.VITE_TITLE}: ${binomialName} (${commonName})`;
+    const pageDescription = `360° macro photography viewer for ${binomialName}`; 
+
     return (
         <>
             <Helmet>
-                <title>{import.meta.env.VITE_TITLE}: {binomialName} ({commonName})</title>
-                <meta name="description" content={`360° macro photography viewer for ${binomialName}`}/>
+                <title>{pageTitle}</title>
+                <meta name="description" content={pageDescription}/>
+
+                {/* Open Graph */}
+                <meta property="og:title" content={pageTitle}/>
+                <meta property="og:description" content={pageDescription}/>
+                <meta property="og:image" content={`https://lookatbugs.com/species_assets/${species.sid}/sequence/${species.sid}_viewer_frame-01.webp`}/>
+                <meta property="og:image:width" content="2400"/>
+                <meta property="og:image:height" content="1800"/>
             </Helmet>
             <SpeciesWindow />
         </>
