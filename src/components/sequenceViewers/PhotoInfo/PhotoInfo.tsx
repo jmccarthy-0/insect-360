719d411fcc349a534dce1bef7c4a8dd02e6e28db
Joe M <mccarthysound@gmail.com>
2024-01-20 15:26:24 -0500

    Remove unused variable

--- src/components/sequenceViewers/PhotoInfo/PhotoInfo.tsx
+++ src/components/sequenceViewers/PhotoInfo/PhotoInfo.tsx
@@ -6,7 +6,7 @@ const PhotoInfo = ({ content }: PhotoInfoProps) => {
     const parseHtml = (str: string) => { return {__html: str} };
     
     return (
-        <div className={`${PhotoInfoStyles['photo-info']} w-full pt-16 px-page-x pb-4 text-primary-dark dark:text-primary-light bg-primary-light dark:bg-primary-dark [&_dl]:grid [&_dl]:grid-cols-max-1fr [&_dl]:gap-x-4 [&_dl]:gap-y-2 [&_p]:text-base not-last:[&_p]:mb-4`} dangerouslySetInnerHTML={parseHtml(content)}>
+        <div className='w-full pt-16 px-page-x pb-4 text-primary-dark dark:text-primary-light bg-primary-light dark:bg-primary-dark [&_dl]:grid [&_dl]:grid-cols-max-1fr [&_dl]:gap-x-4 [&_dl]:gap-y-2 [&_p]:text-base not-last:[&_p]:mb-4' dangerouslySetInnerHTML={parseHtml(content)}>
         </div>
     )
 }
