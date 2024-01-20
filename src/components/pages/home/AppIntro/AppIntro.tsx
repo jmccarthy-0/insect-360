78199aea971676a56a9f23642fc11ebd31d49d47
Joe M <mccarthysound@gmail.com>
2024-01-19 23:28:23 -0500

    Refine app intro layout

--- src/components/pages/home/AppIntro/AppIntro.tsx
+++ src/components/pages/home/AppIntro/AppIntro.tsx
@@ -24,8 +24,8 @@ const AppIntro = () => {
     return (
         <div className='grid gap-y-8 place-content-center text-center'>
             <div className={`relative lg:px-8 2xl:px-16 lg:py-4 2xl:py-6 after:content-none after:lg:content-[''] after:h-full after:absolute after:top-0 after:left-px after:border-l after:border-muted-dark dark:after:border-muted-light after:origin-top ${showBorder ? 'after:scale-y-1' : 'after:scale-y-0'} after:transition-transform after:duration-300 after:ease-in`}>
-                <h1 className='text-5xl md:text-6xl lg:text-8xl text-primary-dark dark:text-primary-light mb-6 md:mb-8 lg:mb-10'>{import.meta.env.VITE_TITLE}</h1> 
-                <p className='text-base mb-12 md:mb-16 text-primary-dark dark:text-primary-light'>Explore high resolution insect anatomy from every angle with LAB. Select a species to get started.</p>
+                <h1 className='text-5xl md:text-6xl lg:text-8xl text-primary-dark dark:text-primary-light mb-6 lg:mb-8'>{import.meta.env.VITE_TITLE}</h1> 
+                <p className='text-base mb-12 md:mb-16 md:max-w-xl lg:max-w-full text-primary-dark dark:text-primary-light'>Explore high resolution insect anatomy from every angle with LAB. Select a species to get started.</p>
                 
                 <div className='flex justify-center'>
                     <Btn handleClick={handleClick}>
