c70882cb03db528d0f4fedece0e37fa5786f539c
Joe M <mccarthysound@gmail.com>
2024-01-20 00:30:01 -0500

    Convert 404 page to tailwind

--- src/components/global/Error404/Error404.tsx
+++ src/components/global/Error404/Error404.tsx
@@ -1,11 +1,9 @@
-import classes from './Error404.module.css';
-
 const Error404 = () => {
     return (
         <div className='grid w-[95%] min-h-dvh mx-auto grid-rows-max-1fr'>
-            <main className={`${classes['error-wrapper']}`}>
-                <h1 className={`${classes['error-title']}`}>404</h1>
-                <p className={`${classes['error-descr']}`}>Page not found.</p>
+            <main className='text-center w-full h-dvh grid place-content-center bg-primary-light'>
+                <h1 className='text-primary-dark text-5xl md:text-6xl lg:text-8xl mb-4'>404</h1>
+                <p className='text-primary-dark'>Page not found.</p>
             </main>
         </div>
     );
