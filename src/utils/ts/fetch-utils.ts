93bc72e740936832845045845ed3456f4badacdf
Joe M <mccarthysound@gmail.com>
2024-01-28 16:48:47 -0500

    Update fetch for new  payload

--- src/utils/ts/fetch-utils.ts
+++ src/utils/ts/fetch-utils.ts
@@ -3,7 +3,7 @@ export const fetchData = async (url: string) => {
     const response = await fetch(url);
     const data = await response.json();
 
-    return data;
+    return data.body;
   } catch (e) {
     console.log({ url });
     console.log(e);
