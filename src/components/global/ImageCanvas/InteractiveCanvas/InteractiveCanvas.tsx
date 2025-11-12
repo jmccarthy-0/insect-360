156d1e5ce2c5edd3e56ee91382b9d87deb794cac
Joe M <mccarthysound@gmail.com>
2025-11-11 23:09:01 -0500

    Test husky

--- src/components/global/ImageCanvas/InteractiveCanvas/InteractiveCanvas.tsx
+++ src/components/global/ImageCanvas/InteractiveCanvas/InteractiveCanvas.tsx
@@ -1,72 +1,73 @@
-import ZoomBtns from "@components/sequenceViewers/ZoomBtns/ZoomBtns";
+import ZoomBtns from '@components/sequenceViewers/ZoomBtns/ZoomBtns';
 
-import { useEffect, useRef } from "react";
-import { useGesture } from "@use-gesture/react";
+import { useEffect, useRef } from 'react';
+import { useGesture } from '@use-gesture/react';
 
-import { InteractiveImageCanvas } from "@utils/ts/ImageCanvas";
-
+import { InteractiveImageCanvas } from '@utils/ts/ImageCanvas';
 
 interface InteractiveCanvasProps {
-  img: HTMLImageElement | ImageBitmap | null;
+    img: HTMLImageElement | ImageBitmap | null;
 }
 
 const InteractiveCanvas = ({ img }: InteractiveCanvasProps) => {
-  const canvasElemRef = useRef<null | HTMLCanvasElement>(null);
-  const canvasRef = useRef<null | InteractiveImageCanvas>(null)
+    const canvasElemRef = useRef<null | HTMLCanvasElement>(null);
+    const canvasRef = useRef<null | InteractiveImageCanvas>(null);
+    const test = 4;
 
-  const bindGesture = useGesture({
-    onDrag: ({delta, pinching, touches}) => {
-      // Pinch takes precedent over drag
-      if (pinching || touches > 1) {
-        return
-      }
+    const bindGesture = useGesture({
+        onDrag: ({ delta, pinching, touches }) => {
+            // Pinch takes precedent over drag
+            if (pinching || touches > 1) {
+                return;
+            }
 
-      if (canvasRef.current && !canvasRef.current.isAnimating) {
-        canvasRef.current.handleDrag(delta);
-      }
-    },
-    
-    onPinch: ({event, origin, delta}) => {
-      event.preventDefault();
-      
-      if (canvasRef.current && !canvasRef.current.isAnimating) {
-        canvasRef.current.handlePinch(delta[0], origin)
-      }
-    },
-    onPinchEnd: () => {
-      if (canvasRef.current && !canvasRef.current.isAnimating) {
-        canvasRef.current.animateRecenter();
-      }
-    }
-  })
+            if (canvasRef.current && !canvasRef.current.isAnimating) {
+                canvasRef.current.handleDrag(delta);
+            }
+        },
 
+        onPinch: ({ event, origin, delta }) => {
+            event.preventDefault();
 
-  useEffect(() => {
-      if (canvasElemRef.current && !canvasRef.current) {
-        canvasRef.current = new InteractiveImageCanvas(canvasElemRef.current);
-      }
-  
-      if (canvasRef.current && img) {
-        canvasRef.current.img = img;
-      }
-  
-      return () => {
-        if (canvasRef.current) {
-          canvasRef.current.destroy();
+            if (canvasRef.current && !canvasRef.current.isAnimating) {
+                canvasRef.current.handlePinch(delta[0], origin);
+            }
+        },
+        onPinchEnd: () => {
+            if (canvasRef.current && !canvasRef.current.isAnimating) {
+                canvasRef.current.animateRecenter();
+            }
+        },
+    });
+
+    useEffect(() => {
+        if (canvasElemRef.current && !canvasRef.current) {
+            canvasRef.current = new InteractiveImageCanvas(
+                canvasElemRef.current
+            );
         }
-      }
-  }, [img])
 
-  return (
-    <div className="h-dvh w-dvw overscroll-none">
-      <ZoomBtns canvasRef={canvasRef} />
-      <canvas
-        className={`h-full w-full bg-primary-dark touch-none`}
-        ref={canvasElemRef}
-        {...bindGesture()}
-      ></canvas>
-    </div>
-  );
+        if (canvasRef.current && img) {
+            canvasRef.current.img = img;
+        }
+
+        return () => {
+            if (canvasRef.current) {
+                canvasRef.current.destroy();
+            }
+        };
+    }, [img]);
+
+    return (
+        <div className="h-dvh w-dvw overscroll-none">
+            <ZoomBtns canvasRef={canvasRef} />
+            <canvas
+                className={`h-full w-full touch-none bg-primary-dark`}
+                ref={canvasElemRef}
+                {...bindGesture()}
+            ></canvas>
+        </div>
+    );
 };
 
 export default InteractiveCanvas;
